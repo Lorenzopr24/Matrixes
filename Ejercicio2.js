9e833c7fdf1bb10bfa7b36542284d16d833fdb91
Lorenzopr24 <d48068702@alumnos.itr.edu.ar>
2023-06-16 16:59:27 -0300

    completed

--- Ejercicio2.js
+++ Ejercicio2.js
@@ -1,33 +1,22 @@
-function SumBorders(matrix){
-let lan=matrix[0].length;
-let lon=matrix.length;
-let pan=(matrix[0].length)-1;
-let pon=(matrix.length)-1;
-let result=[];
-let suma=(matrix[pon].length)-1;
-let resta=(matrix.length)-2
-for(let y=0;y<lon;y++){
-    for(let x=0;x<lan;x++){
-if(y===0){
-result.push(matrix[y][x]);
-
-}else if(y<pon && y>0 &&x===pan){
-result.push(matrix[y][x]);
-
-}else if(y===pon){
-result.push(matrix[y][suma])
-suma--;
-}else if(y>pon && y>0 &&x===pan){
-    result.push(matrix[y][x]);
-}
-
-    } 
-return(result);
-}
-
-let matriz=[[1,2,3,4],
-            [5,6,7,8],
-            [9,10,11,12],
-            [13,14,15,16]]
-
-            console.log(SumBorders(matriz));
+function sumBorders(matrix) {
+    let result = 0;
+  
+    for (let y = 0; y < matrix.length; y++) {
+      for (let x = 0; x < matrix[y].length; x++) {
+        if (y === 0 || y === matrix.length - 1 || x === 0 || x === matrix[y].length - 1) {
+          result+=matrix[y][x];
+        }
+      }
+    }
+  
+    return result;
+  }
+  
+  let matriz = [
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12],
+    [13, 14, 15, 16]
+  ];
+  
+  console.log(sumBorders(matriz));
