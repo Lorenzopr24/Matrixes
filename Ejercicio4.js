4e16c18fd8713782b1383d23a5b892eda01f279b
Lorenzopr24 <d48068702@alumnos.itr.edu.ar>
2023-06-17 15:44:16 -0300

    completed

--- Ejercicio4.js
+++ Ejercicio4.js
@@ -1,34 +1,50 @@
 function spiralPrint(matrix) {
-    let result = [];
+    const rows = matrix.length; // Número de filas de la matriz
+    const columns = matrix[0].length; // Número de columnas de la matriz
+    let top = 0; // Índice de la fila superior
+    let bottom = rows - 1; // Índice de la fila inferior
+    let left = 0; // Índice de la columna izquierda
+    let right = columns - 1; // Índice de la columna derecha
+    let result = []; // Array para almacenar los elementos impresos
   
-    for (let y = 0; y < matrix.length; y++) {
-      for (let x = 0; x < matrix[y].length; x++) {
-        if(y===0){
-       result.push(matrix[y][x]);
-
+    while (top <= bottom && left <= right) {
+      // Imprimir fila superior
+      for (let i = left; i <= right; i++) {
+        result.push(matrix[top][i]);
+      }
+      top++;
+  
+      // Imprimir columna derecha
+      for (let i = top; i <= bottom; i++) {
+        result.push(matrix[i][right]);
+      }
+      right--;
+  
+      if (top <= bottom) {
+        // Imprimir fila inferior
+        for (let i = right; i >= left; i--) {
+          result.push(matrix[bottom][i]);
         }
-        
+        bottom--;
+      }
+  
+      if (left <= right) {
+        // Imprimir columna izquierda
+        for (let i = bottom; i >= top; i--) {
+          result.push(matrix[i][left]);
+        }
+        left++;
       }
     }
   
-    return result;
+    return result; // Devolver los elementos impresos en forma de array
   }
   
-  let matriz = [
-    [1, 2, 3, 4],
-    [5, 6, 7, 8],
-    [9, 10, 11, 12],
-    [13, 14, 15, 16]
-  ];
-  
-  console.log(spiralPrint(matriz));
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+  console.log(
+    spiralPrint([
+      [1, 2, 3,6],
+      [4, 5, 6,6],
+      [7, 8, 9,6],
+      [8, 9, 0,0]
+    ])
+  );
